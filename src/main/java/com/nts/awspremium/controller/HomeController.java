74251c953001dc03ede5964366468e30e1182771
vanthuan203 <44629914+vanthuan203@users.noreply.github.com>
2023-01-17 19:35:38 +0700

    cmc 17.11.2023

--- src/main/java/com/nts/awspremium/controller/HomeController.java
+++ src/main/java/com/nts/awspremium/controller/HomeController.java
@@ -40,7 +40,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        return "Vocuc203 || AccPremium + Proxy 8002";
+        return "Vocuc203 || AccPremium + Proxy 8003";
     }
 
     //Get video tren channel
