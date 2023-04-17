197113cf28837b2ae798eaafeab6bad5c79b1149
vanthuan203 <44629914+vanthuan203@users.noreply.github.com>
2023-04-18 03:48:42 +0700

    cmc 12/4/2023

--- src/main/java/com/nts/awspremium/repositories/HistoryViewRepository.java
+++ src/main/java/com/nts/awspremium/repositories/HistoryViewRepository.java
@@ -42,7 +42,7 @@ public interface HistoryViewRepository extends JpaRepository<HistoryView,Long> {
     @Query(value = "update historyview set running=0,videoid='' where running=1 and POSITION(videoid in listvideo)=0 and  round((UNIX_TIMESTAMP()-timeget/1000)/60)>=20",nativeQuery = true)
     public Integer resetThreadcron();
 
-    @Query(value = "select * from INFORMATION_SCHEMA.PROCESSLIST where db = 'AccPremium' and COMMAND='Query' and STATE not in('preparing','updating')",nativeQuery = true)
+    @Query(value = "select count(*) from INFORMATION_SCHEMA.PROCESSLIST where db = 'AccPremium' and COMMAND='Query' and STATE not in('preparing','updating')",nativeQuery = true)
     public Integer PROCESSLISTVIEW();
     @Modifying
     @Transactional
