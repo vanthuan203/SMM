ddf78a1f849ba10ca5abdf540f34a119e1c5a244
vocuc203 <nguyenthuan281294@gmail.com>
2023-07-26 15:06:01 +0700

    20/6/2023

--- src/main/java/com/nts/awspremium/controller/HistoryViewController.java
+++ src/main/java/com/nts/awspremium/controller/HistoryViewController.java
@@ -353,10 +353,22 @@ public class HistoryViewController {
                     histories.get(0).setOrderid(videos.get(0).getOrderid());
                     histories.get(0).setChannelid(videos.get(0).getChannelid());
                 } else {
-                    resp.put("status", "fail");
-                    resp.put("fail", "video");
-                    resp.put("message", "Không còn video để view!");
-                    return new ResponseEntity<String>(resp.toJSONString(), HttpStatus.OK);
+                    if (histories.get(0).getGeo().equals("vn")) {
+                        videos = videoViewRepository.getvideoBuffHVer2VNTEST(histories.get(0).getListvideo(), orderTrue.getValue());
+                    } else if (histories.get(0).getGeo().equals("us")) {
+                        videos = videoViewRepository.getvideoBuffHVer2USTEST(histories.get(0).getListvideo(), orderTrue.getValue());
+                    }
+                    if (videos.size() > 0) {
+                        histories.get(0).setTimeget(System.currentTimeMillis());
+                        histories.get(0).setVideoid(videos.get(0).getVideoid());
+                        histories.get(0).setOrderid(videos.get(0).getOrderid());
+                        histories.get(0).setChannelid(videos.get(0).getChannelid());
+                    } else {
+                        resp.put("status", "fail");
+                        resp.put("fail", "video");
+                        resp.put("message", "Không còn video để view!");
+                        return new ResponseEntity<String>(resp.toJSONString(), HttpStatus.OK);
+                    }
                 }
 
                 Service service = serviceRepository.getInfoService(videos.get(0).getService());
