41ca437c034fd0c9b9f4695be86e07044f43627b
vocuc203 <nguyenthuan281294@gmail.com>
2023-10-05 10:11:40 +0700

    29/9/2023

--- src/main/java/com/nts/awspremium/repositories/VideoViewHistoryRepository.java
+++ src/main/java/com/nts/awspremium/repositories/VideoViewHistoryRepository.java
@@ -68,7 +68,7 @@ public interface VideoViewHistoryRepository extends JpaRepository<VideoViewHisto
     @Query(value = "select videoid from videoviewhistory where timestart!=0 and timecheckbh=0 and cancel=0 and DATE_FORMAT(FROM_UNIXTIME((timestart-3*60*60*1000+24*3*60*60*1000) / 1000), '%Y-%m-%d')=DATE_FORMAT(FROM_UNIXTIME((UNIX_TIMESTAMP())), '%Y-%m-%d') and service in(select service from service where checktime=1) order by timestart asc limit ?1",nativeQuery = true)
     public List<String> getVideoViewHistoriesCheckViewEnd(Integer limit);
 
-    @Query(value = "select videoid from videoviewhistory where timestart!=0 and timecheckbh=0 and cancel=0 and viewend=0 and round((UNIX_TIMESTAMP()-enddate/1000)/60/60)>5 and service in(select service from service where checktime=1) order by enddate asc limit ?1",nativeQuery = true)
+    @Query(value = "select videoid from videoviewhistory where timestart!=0 and timecheckbh=0 and cancel=0 and viewend=0 and round((UNIX_TIMESTAMP()-enddate/1000)/60/60)>8 and service in(select service from service where checktime=1) order by enddate asc limit ?1",nativeQuery = true)
     public List<String> getVideoViewHistoriesCheckViewEndThan5h(Integer limit);
 
 
