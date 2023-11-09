871c7ac704f3eeb77a483a406cefb2802b1ea1a1
vocuc203 <nguyenthuan281294@gmail.com>
2023-11-09 11:28:36 +0700

    2/11/2023 update maxthreads

--- src/main/java/com/nts/awspremium/controller/ApiCmtController.java
+++ src/main/java/com/nts/awspremium/controller/ApiCmtController.java
@@ -203,8 +203,8 @@ public class ApiCmtController {
                 OkHttpClient client1 = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS).writeTimeout(10, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).build();
 
                 Request request1 = null;
-
-                request1 = new Request.Builder().url("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyClOKa8qUz3MJD1RKBsjlIDR5KstE2NmMY&fields=items(id,snippet(title,channelId,liveBroadcastContent),statistics(commentCount),contentDetails(duration))&part=snippet,statistics,contentDetails&id=" + videolist).get().build();
+                //AIzaSyClOKa8qUz3MJD1RKBsjlIDR5KstE2NmMY
+                request1 = new Request.Builder().url("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyDU89b2Gk7nMVj-SPZh8Waq7TasA6KWoWQ&fields=items(id,snippet(title,channelId,liveBroadcastContent),statistics(commentCount),contentDetails(duration))&part=snippet,statistics,contentDetails&id=" + videolist).get().build();
 
                 Response response1 = client1.newCall(request1).execute();
 
