486894310bab964c54758cafba2d425a95bfdba5
vocuc203 <nguyenthuan281294@gmail.com>
2023-12-11 16:03:27 +0700

    25/11/2023 update maxthreads

--- src/main/java/com/nts/awspremium/controller/VideoViewController.java
+++ src/main/java/com/nts/awspremium/controller/VideoViewController.java
@@ -652,7 +652,7 @@ public class VideoViewController {
             Integer CountTheadSetRunningByService=videoViewRepository.getCountThreadSetByCheckTimeVN();
             Integer CountTheadSetRunningByGeo=videoViewRepository.getSumThreadSetByGeo(service.getGeo());
             Integer CountTheadVPSByGeo=vpsRepository.getSumThreadsByGeo(service.getGeo());
-            if(hour>13?(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax()*0.75)):(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax()))){
+            if(hour>13?(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax()*0.65)):(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax()))){
                 break;
             }
             Integer limitService=limitServiceRepository.getLimitRunningByServiceAndUser(videoViews.get(i).getUser().trim(),videoViews.get(i).getService());
@@ -663,7 +663,7 @@ public class VideoViewController {
                         (CountOrderDoneByServiceAndUserInOneDay==null?0:CountOrderDoneByServiceAndUserInOneDay):
                         (CountOrderRunningByUserAndService+(CountOrderDoneByServiceAndUserInOneDay==null?0:CountOrderDoneByServiceAndUserInOneDay)))>=limitService*service.getMax())
                         ||limitService==0
-                        ||(hour>13?(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax()*0.75)):(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax())))){
+                        ||(hour>13?(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax()*0.65)):(CountTheadSetRunningByService==null?false:CountTheadSetRunningByService<CountTheadVPSByGeo*0.15?false:(CountOrderRunningByService==null?false:CountOrderRunningByService>=setting.getMaxorderbuffhvn()*service.getMax())))){
                     continue;
                 }
             }
