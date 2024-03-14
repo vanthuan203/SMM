c8eba0d523375f8181257f9203015b3c0a25b7a9
vocuc203 <nguyenthuan281294@gmail.com>
2024-03-14 18:12:38 +0700

    2/28/2023 update

--- src/main/java/com/nts/awspremium/controller/HistoryViewController.java
+++ src/main/java/com/nts/awspremium/controller/HistoryViewController.java
@@ -357,7 +357,7 @@ public class HistoryViewController {
                         histories.get(0).setOrderid(videos.get(0).getOrderid());
                         histories.get(0).setChannelid(videos.get(0).getChannelid());
                     } else {
-                        videos = videoViewRepository.getvideoBuffHByGeo(geo_rand, histories.get(0).getListvideo(), orderSpeedTrue.getValue());
+                        videos = videoViewRepository.getvideoViewByGeo(geo_rand, histories.get(0).getListvideo(), orderSpeedTrue.getValue());
                         if (videos.size() > 0) {
                             histories.get(0).setTimeget(System.currentTimeMillis());
                             histories.get(0).setVideoid(videos.get(0).getVideoid());
