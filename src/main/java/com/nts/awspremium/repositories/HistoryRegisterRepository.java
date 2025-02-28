16350f344e35f8e44aca99e1cdba2ca13c17df32
vocuc203 <nguyenthuan281294@gmail.com>
2025-02-28 19:03:52 +0700

    update 003 20/11/2024

--- src/main/java/com/nts/awspremium/repositories/HistoryRegisterRepository.java
+++ src/main/java/com/nts/awspremium/repositories/HistoryRegisterRepository.java
@@ -12,6 +12,6 @@ public interface HistoryRegisterRepository extends JpaRepository<HistoryRegister
     @Query(value = "select count(*) from history_register where platform=?1 and profile_id=?2 and round((UNIX_TIMESTAMP()-update_time/1000)/60/60)<24",nativeQuery = true)
     public Integer count_Register_24h_By_Platform_And_ProfileId(String platform,String profile_id);
 
-    @Query(value = "select count(*) from history_register where platform=?1 and round((UNIX_TIMESTAMP()-update_time/1000)/60)<?2",nativeQuery = true)
-    public Integer count_Register_By_Platform_And_Time(String platform,Integer minute);
+    @Query(value = "select count(*) from HistoryRegister h JOIN FETCH ProfileTask p where h.platform=?1 and h.profileTask.device.ip_address=p.device.ip_address and (?3-h.update_time)/1000/60<?2")
+    public Integer count_Register_By_Platform_And_Time(String platform,Integer minute,Long now);
 }
