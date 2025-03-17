fe1c2ff6d75263eb10e4fae746840cad3f60f8fd
vocuc203 <nguyenthuan281294@gmail.com>
2025-03-17 11:41:25 +0700

    update 003 20/11/2024

--- src/main/java/com/nts/awspremium/model/UpdateTaskRequest.java
+++ src/main/java/com/nts/awspremium/model/UpdateTaskRequest.java
@@ -7,10 +7,13 @@ import lombok.Setter;
 @Setter
 public class UpdateTaskRequest {
     private String account_id="";
+    private String device_id="";
+    private String profile_id="";
     private Boolean status=false;
     private Boolean success=null;
     private String task="";
     private String task_key="";
+    private Long order_id=0L;
     private String platform="";
     private Integer isLogin=-1;
     private Integer viewing_time=0;
