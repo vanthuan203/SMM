46881faf085b83d7797156b9f34e615a739c26aa
vocuc203 <nguyenthuan281294@gmail.com>
2025-03-18 14:56:07 +0700

    update 003 20/11/2024

--- src/main/java/com/nts/awspremium/controller/TaskController.java
+++ src/main/java/com/nts/awspremium/controller/TaskController.java
@@ -1336,7 +1336,7 @@ public class TaskController {
                     }else  if(updateTaskRequest.getTask().toLowerCase().trim().equals("share")&&updateTaskRequest.getStatus()==true){
                         tiktokUpdate.tiktok_share(updateTaskRequest.getAccount_id().trim()+"|"+updateTaskRequest.getPlatform().trim(),updateTaskRequest.getTask_key().trim());
                     }else  if(updateTaskRequest.getTask().toLowerCase().trim().equals("favorites")&&updateTaskRequest.getStatus()==true){
-                        tiktokUpdate.tiktok_view(updateTaskRequest.getAccount_id().trim()+"|"+updateTaskRequest.getPlatform().trim(),updateTaskRequest.getTask_key().trim());
+                        tiktokUpdate.tiktok_favorites(updateTaskRequest.getAccount_id().trim()+"|"+updateTaskRequest.getPlatform().trim(),updateTaskRequest.getTask_key().trim());
                     }
                 }else if(platform_Check.equals("facebook")){
                     if(updateTaskRequest.getTask().toLowerCase().trim().equals("follower")&&updateTaskRequest.getStatus()==true){
