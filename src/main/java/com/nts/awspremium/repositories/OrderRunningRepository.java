767b9e4d0b66bc74e1941d80ec96a79238061f21
vocuc203 <nguyenthuan281294@gmail.com>
2025-03-20 02:05:15 +0700

    update 003 20/11/2024

--- src/main/java/com/nts/awspremium/repositories/OrderRunningRepository.java
+++ src/main/java/com/nts/awspremium/repositories/OrderRunningRepository.java
@@ -34,10 +34,10 @@ public interface OrderRunningRepository extends JpaRepository<OrderRunning,Long>
     @Query(value = "SELECT count(*) from order_running where check_count=1 and order_id=?1",nativeQuery = true)
     public Integer check_Check_Count(Long order_id);
 
-    @Query(value = "SELECT o from OrderRunning o JOIN FETCH o.service where o.service.check_count=1 and o.total>0 and o.start_time>0 and (o.update_current_time<o.update_time or (?1-o.update_current_time)/1000/60>=30)")
+    @Query(value = "SELECT o from OrderRunning o JOIN FETCH o.service where o.service.check_count=1 and o.total>0 and o.start_time>0 and (o.update_current_time+1000*60<o.update_time or (?1-o.update_current_time)/1000/60>=30)")
     public List<OrderRunning> get_Order_By_Check_Count(Long now);
 
-    @Query(value = "SELECT o from OrderRunning o JOIN FETCH o.service WHERE o.check_count=?1")
+    @Query(value = "SELECT o from OrderRunning o JOIN FETCH o.service WHERE o.check_count=?1 ORDER BY o.update_current_time ASC")
     public List<OrderRunning> get_Order_By_Check_Count_Num(Integer check_count);
     @Query(value = "SELECT count(*) from order_running where order_key=?1 and service_id in(select service_id from service where task=?2)",nativeQuery = true)
     public Integer get_Order_By_Order_Key_And_Task(String order_id,String task);
