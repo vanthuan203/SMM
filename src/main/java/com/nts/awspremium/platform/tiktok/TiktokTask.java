5bf169bfe6028986c83af66dae442b1fd6f1d313
vocuc203 <nguyenthuan281294@gmail.com>
2025-04-30 21:57:18 +0700

    update 003 20/11/2024

--- src/main/java/com/nts/awspremium/platform/tiktok/TiktokTask.java
+++ src/main/java/com/nts/awspremium/platform/tiktok/TiktokTask.java
@@ -590,7 +590,7 @@ public class TiktokTask {
                 orderRunning = orderRunningRepository.get_Order_Running_By_Task("tiktok","view",mode,"",orderThreadCheck.getValue());
             }
             if(orderRunning==null){
-                orderRunning = orderRunningRepository.get_Order_Running_By_Task("tiktok","view",mode,"",orderThreadSpeedUpCheck.getValue());
+                orderRunning = orderRunningRepository.get_Order_Running_By_Task("tiktok","view","auto","",orderThreadSpeedUpCheck.getValue());
             }
             if (orderRunning!=null) {
                 Service service=orderRunning.getService();
