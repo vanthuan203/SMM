7434e71733fdc745316406ebc1c43b016c549b19
vocuc203 <nguyenthuan281294@gmail.com>
2025-05-12 16:59:48 +0700

    update 003 20/11/2024

--- src/main/java/com/nts/awspremium/controller/TaskController.java
+++ src/main/java/com/nts/awspremium/controller/TaskController.java
@@ -3208,7 +3208,6 @@ public class TaskController {
                     resp.put("data",data);
                     return new ResponseEntity<>(resp, HttpStatus.OK);
                 }
-                /*
                 List<String> ip_List=deviceRepository.get_IP_Running_Task_By_OrderId(Long.parseLong(dataJson.get("order_id").toString()));
                 if(ip_List.size()!=0){
                     Set<String> ipSet = new HashSet<>(ip_List);
@@ -3219,7 +3218,6 @@ public class TaskController {
                         return new ResponseEntity<>(resp, HttpStatus.OK);
                     }
                 }
-                 */
                 dataJson.put("name",accountProfileRepository.get_Name_By_AccountId(profileTask.getAccount_id()));
                 dataJson.put("avatar",accountProfileRepository.get_Avatar_By_AccountId(profileTask.getAccount_id())==0?false:true);
                 dataJson.put("account_id",dataJson.get("account_id").toString().substring(0,dataJson.get("account_id").toString().indexOf("|")));
