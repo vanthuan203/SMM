eb3fcf449e922414c4ae4723411959ddc000dd9a
vocuc203 <nguyenthuan281294@gmail.com>
2025-05-23 18:41:31 +0700

    update 003 20/11/2024

--- src/main/java/com/nts/awspremium/platform/tiktok/TiktokTask.java
+++ src/main/java/com/nts/awspremium/platform/tiktok/TiktokTask.java
@@ -244,7 +244,7 @@ public class TiktokTask {
                         orderRunningRepository.save(orderRunning);
                     }
                 }
-                data.put("channel_id","");
+                data.put("channel_id",orderRunning.getChannel_id().trim());
                 data.put("app", service.getApp());
                 data.put("task_key",orderRunning.getOrder_key());
                 DataFollowerTiktok dataFollowerTiktok=dataFollowerTiktokRepository.get_Data_Follower(orderRunning.getOrder_id());
