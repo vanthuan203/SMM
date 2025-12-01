cab930e2338dd351a2f7a85d2c156ca0f778ca71
vocuc203 <nguyenthuan281294@gmail.com>
2025-12-01 17:42:07 +0700

    update 003 13/11/2025

--- src/main/java/com/nts/awspremium/controller/SettingController.java
+++ src/main/java/com/nts/awspremium/controller/SettingController.java
@@ -392,9 +392,9 @@ public class SettingController {
                 settingSystemRepository.save(settingSystem);
             }else if(thread_running>thread_set){
                 if(thread_running<thread_set*3){
-                    settingSystem.setTime_waiting_task((settingSystem.getTime_waiting_task()-5)>0?(settingSystem.getTime_waiting_task()-5):0);
+                    settingSystem.setTime_waiting_task((settingSystem.getTime_waiting_task()-10)>0?(settingSystem.getTime_waiting_task()-10):0);
                 }else if(thread_running>thread_set*3.5){
-                    settingSystem.setTime_waiting_task((settingSystem.getTime_waiting_task()+5)<500?(settingSystem.getTime_waiting_task()+5):500);
+                    settingSystem.setTime_waiting_task((settingSystem.getTime_waiting_task()+10)<650?(settingSystem.getTime_waiting_task()+10):650);
                 }
                 if(thread_running>settingSystem.getMax_thread()){
                     settingSystem.setMax_thread((settingSystem.getMax_thread()+10)<settingSystem.getMax_thread_fix()?(settingSystem.getMax_thread()+10):settingSystem.getMax_thread_fix());
