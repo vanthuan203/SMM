e646a9b046550d5a9dbec93a44803cad47426dff
vocuc203 <nguyenthuan281294@gmail.com>
2026-01-08 18:06:47 +0700

    update 003 13/11/2025

--- src/main/java/com/nts/awspremium/controller/TaskController.java
+++ src/main/java/com/nts/awspremium/controller/TaskController.java
@@ -650,11 +650,6 @@ public class TaskController {
                         return new ResponseEntity<>(resp, HttpStatus.OK);
                     }
                 }
-            }else{
-                resp.put("status", false);
-                data.put("message", "Không thực hiện nhiệm vụ");
-                resp.put("data", data);
-                return new ResponseEntity<>(resp, HttpStatus.OK);
             }
 
 
